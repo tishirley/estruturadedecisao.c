b979a8eb7d72dfa56479ad8221346fe85f2bc747
tishirley <vipconvitesjf@gmail.com>
2025-04-08 18:31:34 +0000

    composta

---
+++ decisaocomposta.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int numero1;
+    int numero2;
+
+    if (numero1 > numero2) {
+        // bloco de código a ser executado se a condição for verdadeira
+        printf("O número 1 é maior que o número 2\n");
+
+    }else {
+        // bloco de código a ser executado se a condição for falsa
+        printf("O número 1 é menor que o número 2\n");
+
+    }
+
+
+}
