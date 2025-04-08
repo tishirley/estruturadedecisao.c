6960204a462f00fd1ae8c9b151741f63bcbe288a
tishirley <vipconvitesjf@gmail.com>
2025-04-08 18:38:08 +0000

    percentual2

--- decisaocomposta.c
+++ decisaocomposta.c
@@ -4,15 +4,18 @@ int main(){
     int numero1;
     int numero2;
 
-    if (numero1 > numero2) {
+    numero1 = 10;
+    numero2 = 10;
+
+    if (numero1 >= numero2) {
         // bloco de código a ser executado se a condição for verdadeira
-        printf("O número 1 é maior que o número 2\n");
+        printf("O número 1 é maior ou igual ao número 2\n");
 
     }else {
         // bloco de código a ser executado se a condição for falsa
         printf("O número 1 é menor que o número 2\n");
 
     }
-
+ return 0;
 
 }
