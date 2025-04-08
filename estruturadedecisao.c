ea4ed442392b6ac99da78bd383cebd9fe93f8d81
tishirley <vipconvitesjf@gmail.com>
2025-04-08 12:02:21 +0000

    if

---
+++ estruturadedecisao.c
@@ -0,0 +1,14 @@
+int main (){
+    int numero1, numero2;
+
+    numero1 = 5;
+    numero2 = 5;
+
+    if (numero1 > numero2) {
+        printf("Número 1 é maior que número2\n");
+
+    }
+
+    printf("Fora if");
+
+}
